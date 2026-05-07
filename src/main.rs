9e13eacd3d60b23ea5846cfec2613495f3309344
w0nam <lucas.lefevre1999@outlook.fr>
2026-05-07 14:39:04 +0200

    WiP

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
 use std::io;
 use windows_version::*;
-//use winreg::*;
+use winreg::*;
 
-const WIN_11_MIN_VERSION: u32 = 27000;
-// fn menu_switcher() {
-//     println!("Menu switcher here!");
-// }
+const WIN_11_MIN_VERSION: u32 = 22000; // Lowest build number of W11, lower than that would be W10
 
-// fn key_checker() -> io::Result<()> {
-//     HKCU.open_subkey("Software\\Classes\\CLSID\\{86ca1aa0-34aa-4e8b-a509-50c905bae2a3}")?;
-// }
+fn menu_switcher() {
+    println!("Menu switcher fun achieved!");
+}
+
+fn key_checker() -> io::Result<()> {
+    HKCU.open_subkey("Software\\Classes\\CLSID\\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}")?;
+    Ok(())
+}
 
 fn version_checker() -> io::Result<()> {
     if OsVersion::current().build <= WIN_11_MIN_VERSION {
@@ -24,7 +26,14 @@ fn version_checker() -> io::Result<()> {
 
 fn main() -> io::Result<()> {
     version_checker()?;
-    //key_checker()?;
+    key_checker()?;
 
+    println!(
+        "RegEdit Context Menu Key already present, would you like to revert it to W11 context menu ? (y/N)"
+    );
+    // Forgor how to register user input in rust...
+    // let usr_input = String::new();
+    // println!("{}", usr_input);
+    menu_switcher();
     Ok(())
 }
