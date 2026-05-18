3c6b48af38a70a2d94b25db371e59da4fdfb303d
w0nam <lucas.lefevre1999@outlook.fr>
2026-05-18 11:31:26 +0200

    fixed a typo caused by learning vim.

--- src/main.rs
+++ src/main.rs
@@ -61,7 +61,11 @@ fn restart_explorer() -> io::Result<()> {
 
 fn w11_menu_style() -> io::Result<()> {
     Command::new("REG.exe")
-        .args(["delete", ",", "/f"])
+        .args([
+            "delete",
+            "HKCU\\Software\\Classes\\CLSID\\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}",
+            "/f",
+        ])
         .status()?;
     // Forward the last result
     restart_explorer()
